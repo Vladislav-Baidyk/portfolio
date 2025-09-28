51620c920e136390d5b761bfb6cfba0280571cbe
Vlad Baidyk <102901907+Vladislav-Baidyk@users.noreply.github.com>
2025-09-28 19:33:00 +0200

    Scissors rock and paper

---
+++ Game/src/App.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react/jsx-runtime";
+import Body from "./components/body";
+import "./App.css";
+function App() {
+  return (
+    <Fragment>
+      <Body />
+    </Fragment>
+  );
+}
+export default App;
